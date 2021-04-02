a02bd4001e251873d587525109263c3428d43fea
sanyoni <ksyline1997@gmail.com>
2021-04-02 18:11:07 +0900

    Initial Commit

---
+++ src/main/java/com/newneek_clone_back/NewneekCloneBackApplication.java
@@ -0,0 +1,13 @@
+package com.newneek_clone_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewneekCloneBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewneekCloneBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newneek_clone_back/NewneekCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newneek_clone_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewneekCloneBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
