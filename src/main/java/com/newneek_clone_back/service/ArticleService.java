e3c34bb08ff80db85e555875f0feb65dfdd9ca53
sanyoni <ksyline1997@gmail.com>
2021-04-03 19:26:21 +0900

    ArticleService::create 구현

--- src/main/java/com/newneek_clone_back/service/ArticleService.java
+++ src/main/java/com/newneek_clone_back/service/ArticleService.java
@@ -1,5 +1,7 @@
 package com.newneek_clone_back.service;
 
+import com.newneek_clone_back.dto.ArticleRequestDto;
+import com.newneek_clone_back.entity.Article;
 import com.newneek_clone_back.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class ArticleService {
     private final ArticleRepository articleRepository;
+
+    private final ArticleCategoryService categoryService;
+
+    public Article create(ArticleRequestDto requestDto) {
+        Article newArticle = new Article(requestDto, categoryService);
+        articleRepository.save(newArticle);
+
+        return newArticle;
+    }
 }
