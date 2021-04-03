12009c86fe4cbdd2eb69a8fbcd31d68c4ade14e4
sanyoni <ksyline1997@gmail.com>
2021-04-03 21:42:28 +0900

    Article을 조회하는 Repository와 Service코드 작성

--- src/main/java/com/newneek_clone_back/repository/ArticleRepository.java
+++ src/main/java/com/newneek_clone_back/repository/ArticleRepository.java
@@ -1,8 +1,20 @@
 package com.newneek_clone_back.repository;
 
 import com.newneek_clone_back.entity.Article;
+import com.newneek_clone_back.entity.ArticleCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    Optional<List<Article>> findAllByOrderByCreatedAtDesc();
+
+    Optional<List<Article>> findAllByCategoryOrderByCreatedAtDesc(ArticleCategory category);
+
+    Page<Article> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    Page<Article> findAllByCategoryOrderByCreatedAtDesc(ArticleCategory category, Pageable pageable);
 }
--- src/main/java/com/newneek_clone_back/service/ArticleService.java
+++ src/main/java/com/newneek_clone_back/service/ArticleService.java
@@ -2,9 +2,14 @@ package com.newneek_clone_back.service;
 
 import com.newneek_clone_back.dto.ArticleRequestDto;
 import com.newneek_clone_back.entity.Article;
+import com.newneek_clone_back.entity.ArticleCategory;
 import com.newneek_clone_back.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -18,5 +23,21 @@ public class ArticleService {
         articleRepository.save(newArticle);
 
         return newArticle;
+    }
+
+    public List<Article> findAllByOrderByCreatedAtDesc() {
+        return articleRepository.findAllByOrderByCreatedAtDesc().orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    public List<Article> findAllByCategoryOrderByCreatedAtDesc(ArticleCategory category) {
+        return articleRepository.findAllByCategoryOrderByCreatedAtDesc(category).orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    public Page<Article> findAllByOrderByCreatedAtDesc(Pageable pageable) {
+        return articleRepository.findAllByOrderByCreatedAtDesc(pageable);
+    }
+
+    public Page<Article> findAllByCategoryOrderByCreatedAtDesc(ArticleCategory category, Pageable pageable) {
+        return articleRepository.findAllByCategoryOrderByCreatedAtDesc(category, pageable);
     }
 }
