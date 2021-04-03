7b699ec4101e9fd2e3eaa3c4e28b0670f06071ec
sanyoni <ksyline1997@gmail.com>
2021-04-03 19:30:05 +0900

    ArticleController::createArticle 구현

--- src/main/java/com/newneek_clone_back/controller/ArticleController.java
+++ src/main/java/com/newneek_clone_back/controller/ArticleController.java
@@ -1,10 +1,14 @@
 package com.newneek_clone_back.controller;
 
+import com.newneek_clone_back.dto.ArticleRequestDto;
+import com.newneek_clone_back.entity.Article;
 import com.newneek_clone_back.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
@@ -20,8 +24,10 @@ public class ArticleController {
         return jsonObject.toString();
     }
 
-    /*
-
-     */
+    @ResponseBody
+    @PostMapping("/api/articles")
+    public Article createArticle(@RequestBody ArticleRequestDto requestDto) {
+        return articleService.create(requestDto);
+    }
 
 }
