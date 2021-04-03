8d3d47ea49e17e083ae38c3c0905aa004e934384
SAMSUNG\user <dlaekgml15@naver.com>
2021-04-03 12:14:51 +0900

    second

---
+++ src/main/java/com/newneek_clone_back/Article.java
@@ -0,0 +1,25 @@
+package com.newneek_clone_back;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor//자동생성자
+public class Article extends Timestamped{
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private  String title;
+
+    @Column(nullable = true)
+    private String image;
+
+    @Column(nullable = false)
+    private String contents;
+
+
+}
