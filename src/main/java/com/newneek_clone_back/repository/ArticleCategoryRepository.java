0c0a2060f45235043dd196d9f33286fad9cdf1a2
sanyoni <ksyline1997@gmail.com>
2021-04-03 19:24:44 +0900

    ArticleCategory findByName 구현

--- src/main/java/com/newneek_clone_back/repository/ArticleCategoryRepository.java
+++ src/main/java/com/newneek_clone_back/repository/ArticleCategoryRepository.java
@@ -3,5 +3,8 @@ package com.newneek_clone_back.repository;
 import com.newneek_clone_back.entity.ArticleCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArticleCategoryRepository extends JpaRepository<ArticleCategory, Long> {
+    Optional<ArticleCategory> findByName(String name);
 }
--- src/main/java/com/newneek_clone_back/service/ArticleCategoryService.java
+++ src/main/java/com/newneek_clone_back/service/ArticleCategoryService.java
@@ -1,5 +1,6 @@
 package com.newneek_clone_back.service;
 
+import com.newneek_clone_back.entity.ArticleCategory;
 import com.newneek_clone_back.repository.ArticleCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ArticleCategoryService {
     private final ArticleCategoryRepository categoryRepository;
+
+    public ArticleCategory findByName(String name) {
+        try {
+            return categoryRepository.findByName(name).orElseThrow(() -> new IllegalArgumentException());
+        } catch (IllegalArgumentException e) {
+            ArticleCategory newCategory = new ArticleCategory();
+            newCategory.setName(name);
+
+            return categoryRepository.save(newCategory);
+        }
+
+    }
 }
