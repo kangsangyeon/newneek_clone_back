849e5e0e7211616fb82b04e2b5c1e233816a5699
sanyoni <ksyline1997@gmail.com>
2021-04-04 12:11:45 +0900

    ArticleController에  /api/articles 요청에 대해 응답하는 코드 작성

--- src/main/java/com/newneek_clone_back/controller/ArticleController.java
+++ src/main/java/com/newneek_clone_back/controller/ArticleController.java
@@ -5,21 +5,39 @@ import com.newneek_clone_back.entity.Article;
 import com.newneek_clone_back.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class ArticleController {
+    private final int PAGE_SIZE = 12;
+
     private final ArticleService articleService;
 
     @ResponseBody
     @GetMapping(value = "/api/articles", produces = "application/json")
-    public String getArticles() {
+    public String getArticles(@RequestParam(required = false) String category, @RequestParam(required = false) Integer page) {
+        List<Article> articleList;
+
+        if (page == null) {
+            articleList = category == null ? articleService.findAllByOrderByCreatedAtDesc() : articleService.findAllByCategoryOrderByCreatedAtDesc(category);
+        } else {
+            Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+
+            Page<Article> articlePage = category == null ? articleService.findAllByOrderByCreatedAtDesc(pageable) : articleService.findAllByCategoryOrderByCreatedAtDesc(category, pageable);
+            articleList = articlePage.getContent();
+        }
+
         JSONObject jsonObject = new JSONObject();
+        jsonObject.put("articleList", articleList);
+        jsonObject.put("category", category);
+        jsonObject.put("page", page);
 
         return jsonObject.toString();
     }
