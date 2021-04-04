68ae9c94bf0937f3f42935decf6b67952663c613
sanyoni <ksyline1997@gmail.com>
2021-04-04 13:03:11 +0900

    Article의 목록을 돌려줄 때에는 Article의 내용을 요약한 ArticleSummary를 리턴하도록 했고, ArticleSummary를 조회하는 과정의 코드를 Service로 옮김

--- src/main/java/com/newneek_clone_back/controller/ArticleController.java
+++ src/main/java/com/newneek_clone_back/controller/ArticleController.java
@@ -1,13 +1,11 @@
 package com.newneek_clone_back.controller;
 
 import com.newneek_clone_back.dto.ArticleRequestDto;
+import com.newneek_clone_back.dto.ArticleSummaryResponseDto;
 import com.newneek_clone_back.entity.Article;
 import com.newneek_clone_back.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONObject;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,26 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 @Controller
 public class ArticleController {
-    private final int PAGE_SIZE = 12;
-
     private final ArticleService articleService;
 
     @ResponseBody
     @GetMapping(value = "/api/articles", produces = "application/json")
-    public String getArticles(@RequestParam(required = false) String category, @RequestParam(required = false) Integer page) {
-        List<Article> articleList;
-
-        if (page == null) {
-            articleList = category == null ? articleService.findAllByOrderByCreatedAtDesc() : articleService.findAllByCategoryOrderByCreatedAtDesc(category);
-        } else {
-            Pageable pageable = PageRequest.of(page, PAGE_SIZE);
-
-            Page<Article> articlePage = category == null ? articleService.findAllByOrderByCreatedAtDesc(pageable) : articleService.findAllByCategoryOrderByCreatedAtDesc(category, pageable);
-            articleList = articlePage.getContent();
-        }
+    public String getArticleSummaries(@RequestParam(required = false) String category, @RequestParam(required = false) Integer page) {
+        List<ArticleSummaryResponseDto> articleSummaryList = articleService.getArticleSummuryList(category, page);
 
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("articleList", articleList);
+        jsonObject.put("articleSummaryList", articleSummaryList);
         jsonObject.put("category", category);
         jsonObject.put("page", page);
 
--- src/main/java/com/newneek_clone_back/service/ArticleService.java
+++ src/main/java/com/newneek_clone_back/service/ArticleService.java
@@ -1,19 +1,24 @@
 package com.newneek_clone_back.service;
 
 import com.newneek_clone_back.dto.ArticleRequestDto;
+import com.newneek_clone_back.dto.ArticleSummaryResponseDto;
 import com.newneek_clone_back.entity.Article;
 import com.newneek_clone_back.entity.ArticleCategory;
 import com.newneek_clone_back.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleService {
+    private final int PAGE_SIZE = 12;
+
     private final ArticleRepository articleRepository;
 
     private final ArticleCategoryService categoryService;
@@ -41,5 +46,23 @@ public class ArticleService {
     public Page<Article> findAllByCategoryOrderByCreatedAtDesc(String categoryName, Pageable pageable) {
         ArticleCategory category = categoryService.findByName(categoryName);
         return articleRepository.findAllByCategoryOrderByCreatedAtDesc(category, pageable);
+    }
+
+    public List<ArticleSummaryResponseDto> getArticleSummuryList(String categoryName, Integer page) {
+        List<Article> articleList;
+
+        if (page == null) {
+            articleList = categoryName == null ? findAllByOrderByCreatedAtDesc() : findAllByCategoryOrderByCreatedAtDesc(categoryName);
+        } else {
+            Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+
+            Page<Article> articlePage = categoryName == null ? findAllByOrderByCreatedAtDesc(pageable) : findAllByCategoryOrderByCreatedAtDesc(categoryName, pageable);
+            articleList = articlePage.getContent();
+        }
+
+        List<ArticleSummaryResponseDto> articleSummaryList = new ArrayList<>(articleList.size());
+        articleList.forEach(article -> articleSummaryList.add(new ArticleSummaryResponseDto(article)));
+
+        return articleSummaryList;
     }
 }
